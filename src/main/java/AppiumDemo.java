37f951b528fca21bd4d7241aae0583cc0bbb25ef
Ergin ARICI <ergin.arici@useinsider.com>
2023-03-09 18:59:33 +0300

    initial commit

---
+++ src/main/java/AppiumDemo.java
@@ -0,0 +1,42 @@
+import java.io.File;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.util.concurrent.TimeUnit;
+import io.appium.java_client.remote.MobilePlatform;
+import org.openqa.selenium.remote.DesiredCapabilities;
+import org.openqa.selenium.remote.RemoteWebDriver;
+import org.testng.annotations.BeforeSuite;
+import org.testng.annotations.AfterSuite;
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.Test;
+import io.appium.java_client.MobileElement;
+import io.appium.java_client.TouchAction;
+import io.appium.java_client.android.AndroidDriver;
+import io.appium.java_client.remote.MobileCapabilityType;
+public class AppiumDemo
+{
+    public static URL url;
+    public static DesiredCapabilities capabilities;
+    public static RemoteWebDriver driver;
+
+    @BeforeSuite
+    public  void setupAppium() throws MalformedURLException
+    {
+        DesiredCapabilities capabilities = new DesiredCapabilities();
+        capabilities.setCapability(MobileCapabilityType.PLATFORM_VERSION, "11");
+        capabilities.setCapability(MobileCapabilityType.PLATFORM_NAME, MobilePlatform.ANDROID);
+        capabilities.setCapability("appPackage","com.whatsapp");
+        capabilities.setCapability("appActivity","Main");
+        capabilities.setCapability("autoAcceptAlerts",true);
+        capabilities.setCapability("noReset",true);
+        capabilities.setCapability("fullReset", false);
+        capabilities.setCapability("autoGrantPermissions",true);
+        capabilities.setCapability(MobileCapabilityType.NEW_COMMAND_TIMEOUT, 10000);
+        driver = new RemoteWebDriver(new URL("http://127.0.0.1:4723/wd/hub"), capabilities);
+        driver.manage().timeouts().implicitlyWait(15, TimeUnit.SECONDS);
+    }
+
+    @Test (enabled=true) public void myFirstTest() throws InterruptedException {
+        System.out.println("Test Completed.");
+    }
+}
