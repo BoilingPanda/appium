f9229bedd6dfcbe4c19b9de755f1d968de58d939
Ergin ARICI <ergin.arici@useinsider.com>
2023-03-14 21:14:29 +0300

    new commit

--- src/test/java/AppiumDemo3.java
+++ src/test/java/AppiumDemo3.java
@@ -1,3 +1,4 @@
+import io.appium.java_client.android.AndroidDriver;
 import io.appium.java_client.remote.MobileCapabilityType;
 import io.appium.java_client.remote.MobilePlatform;
 import org.openqa.selenium.remote.DesiredCapabilities;
@@ -13,7 +14,7 @@ public class AppiumDemo3
 {
     public static URL url;
     public static DesiredCapabilities capabilities;
-    public static RemoteWebDriver driver;
+    public static AndroidDriver driver;
 
     @BeforeSuite
     public  void setupAppium() throws MalformedURLException
@@ -28,7 +29,7 @@ public class AppiumDemo3
         capabilities.setCapability("fullReset", false);
         capabilities.setCapability("autoGrantPermissions",true);
         capabilities.setCapability(MobileCapabilityType.NEW_COMMAND_TIMEOUT, 10000);
-        driver = new RemoteWebDriver(new URL("http://10.0.10.5:4727/wd/hub"), capabilities);
+        driver = new AndroidDriver(new URL("http://10.0.10.5:4727/wd/hub"), capabilities);
         driver.manage().timeouts().implicitlyWait(15, TimeUnit.SECONDS);
     }
 
