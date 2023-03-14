d6bbb9198f09707a5fb76a050c7551f15350d667
Ergin ARICI <ergin.arici@useinsider.com>
2023-03-15 01:29:26 +0300

    new commit

--- src/test/java/BaseTest.java
+++ src/test/java/BaseTest.java
@@ -1,3 +1,4 @@
+import io.appium.java_client.AppiumDriver;
 import io.appium.java_client.android.AndroidDriver;
 import io.appium.java_client.remote.MobileCapabilityType;
 import org.openqa.selenium.remote.DesiredCapabilities;
@@ -10,19 +11,19 @@ import java.net.URL;
 
 public class BaseTest {
 
-    public ThreadLocal<AndroidDriver> driver = new ThreadLocal<AndroidDriver>();
+    public ThreadLocal<AppiumDriver> driver = new ThreadLocal<>();
 
-    public void setDriver(AndroidDriver driver){
+    public void setDriver(AppiumDriver driver){
         this.driver.set(driver);
     }
 
-    public AndroidDriver getDriver(){
+    public AppiumDriver getDriver(){
         return this.driver.get();
     }
 
     @BeforeMethod
-    @Parameters({"deviceName", "platformVersion", "portNumber", "udid"})
-    public void initiateDriver(String deviceName, String platformVersion, String portNumber, String udid) throws MalformedURLException {
+    @Parameters({"deviceName", "platformVersion", "portNumber", "udid", "systemPort"})
+    public void initiateDriver(String deviceName, String platformVersion, String portNumber, String udid, String systemPort) throws MalformedURLException {
         DesiredCapabilities capabilities = new DesiredCapabilities();
         capabilities.setCapability("deviceName", deviceName);
         capabilities.setCapability("platformVersion", platformVersion);
@@ -35,7 +36,7 @@ public class BaseTest {
         capabilities.setCapability("autoGrantPermissions",true);
         capabilities.setCapability(MobileCapabilityType.NEW_COMMAND_TIMEOUT, 10000);
         capabilities.setCapability("udid", udid);
-        capabilities.setCapability("systemPort", 8205);
+        capabilities.setCapability("systemPort", systemPort);
         System.out.println("Test Started");
         setDriver(new AndroidDriver(new URL("http://10.0.10.5:"+portNumber+"/wd/hub"), capabilities));
     }
